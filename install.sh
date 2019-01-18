e97ff7ec3699005f4b1d2f83b47050df42d616d4
BreaBrain <46314237+BreaBrain@users.noreply.github.com>
2019-01-18 10:36:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,20 +6,19 @@
 clear
 echo "Do you want to install VNC for vServer? (y/n)"
 read q
-if [ q == "y" ]; then
+if [ $q == "y" ]; then
 #echo "Resoltion [ Width x height ] ? (normal)"
 echo "Different resolution? (y/n)"
-read r 
-if [ r == "y" ]; then
+read r
+if [ $r == "y" ]; then
 echo "width"
 read w
 echo "heigth"
 read h
-res="-geometry " + w + "x" + h 
+res="-geometry $w\x$h"
 fi
-
-sudo apt-get update                                                                                                 
-sudo apt-get -y upgrade                                                                                                
+sudo apt-get update
+sudo apt-get -y upgrade
 sudo apt-get -y install lxde
 sudo apt-get install -y nano
 sudo apt-get install -y xorg lxde-core tightvncserver
@@ -29,7 +28,7 @@ echo "lxterminal &"  >> ~/.vnc/xstartup
 echo "/usr/bin/lxsession -s LXDE &" >> ~/.vnc/xstartup
 
 echo "#!/bin/bash" >> /bin/vncstart
-echo "tightvncserver :1 " + res >> /bin/vncstart
+echo "tightvncserver :1 $res">> /bin/vncstart
 sudo chmod +x /bin/vncstart
 echo "#!/bin/bash" >> /bin/vncstop
 echo "tightvncserver -kill :1" >> /bin/vncstop
@@ -41,10 +40,11 @@ sudo apt-get -y install mpv
 clear
 echo "---------------------------"
 echo ": Finished!               :"
-echo ": VNC IP:   localhost:1   :" 
+echo ": VNC IP:   localhost:1   :"
 echo ": Start:    vncstart      :"
 echo ": Stop:     vncstop       :"
 echo "---------------------------"
-else 
+else
+echo $q
 echo "ok"
 fi
