78ff33010b0f41db57f0a91c300f89b3622b8b16
BreaBrain <46314237+BreaBrain@users.noreply.github.com>
2019-01-18 10:48:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,16 +17,35 @@ echo "heigth"
 read h
 res="-geometry $w\x$h"
 fi
+echo "---OS---"
+echo "1. Debian"
+echo "2. Ubuntu"
+echo "more comming soon.."
+read os
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install lxde
 sudo apt-get install -y nano
-sudo apt-get install -y xorg lxde-core tightvncserver
+if [ $os == "1" ]; then
+sudo apt-get -y install xorg lxde-core tightvncserver
+elif[ $os == "2" ]; then
+sudo apt-get install -y xfce4 xfce4-goodies
+sudo apt-get -y install tightvncserver
+fi
 tightvncserver :1
 tightvncserver -kill :1
+if [ $os == "1" ]; then
 echo "lxterminal &"  >> ~/.vnc/xstartup
 echo "/usr/bin/lxsession -s LXDE &" >> ~/.vnc/xstartup
-
+elif[ $os == "2"]; then
+mv /root/.vnc/xstartup /root/.vnc/xstartup.bak
+echo "
+#!/bin/bash
+xrdb $HOME/.Xresources
+startxfce4 &
+" >> /root/.vnc/xstartup
+sudo chmod +x  /root/.vnc/xstartup
+fi
 echo "#!/bin/bash" >> /bin/vncstart
 echo "tightvncserver :1 $res">> /bin/vncstart
 sudo chmod +x /bin/vncstart
