c8932f122348f27678df7f2dd73cbc00ba1ce41e
BreaBrain <46314237+BreaBrain@users.noreply.github.com>
2019-01-18 09:29:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,21 @@
 #works with:        debian
 #doesn't work with:    opensuse
 #----------------------------
+clear
+echo "Do you want to install VNC for vServer? (y/n)"
+read q
+if [ q == "y" ]; then
+#echo "Resoltion [ Width x height ] ? (normal)"
+echo "Different resolution? (y/n)"
+read r 
+if [ r == "y" ]; then
+echo "width"
+read w
+echo "heigth"
+read h
+res="-geometry " + w + "x" + h 
+fi
+
 sudo apt-get update                                                                                                 
 sudo apt-get -y upgrade                                                                                                
 sudo apt-get -y install lxde
@@ -14,7 +29,7 @@ echo "lxterminal &"  >> ~/.vnc/xstartup
 echo "/usr/bin/lxsession -s LXDE &" >> ~/.vnc/xstartup
 
 echo "#!/bin/bash" >> /bin/vncstart
-echo "tightvncserver :1 " >> /bin/vncstart
+echo "tightvncserver :1 " + res >> /bin/vncstart
 sudo chmod +x /bin/vncstart
 echo "#!/bin/bash" >> /bin/vncstop
 echo "tightvncserver -kill :1" >> /bin/vncstop
@@ -29,3 +44,6 @@ echo ": VNC IP:   localhost:1   :"
 echo ": Start:    vncstart      :"
 echo ": Stop:     vncstop       :"
 echo "---------------------------"
+else 
+echo "ok"
+fi
