191fdc7f4f34e1302d9312039392544dceb4e571
BreaBrain <46314237+BreaBrain@users.noreply.github.com>
2019-01-18 09:03:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash                                                                                                         
 sudo apt-get update                                                                                                 
-sudo apt-get upgrade                                                                                                
-sudo apt-get install lxde
-apt-get install nano
-apt-get install xorg lxde-core tightvncserver
+sudo apt-get -y upgrade                                                                                                
+sudo apt-get -y install lxde
+sudo apt-get install -y nano
+sudo apt-get install -y xorg lxde-core tightvncserver
 tightvncserver :1
 tightvncserver -kill :1
 echo "lxterminal &"  >> ~/.vnc/xstartup
