2e1911bdbfa18d075238b59ca2ddbf8adc159171
BreaBrain <46314237+BreaBrain@users.noreply.github.com>
2019-01-24 15:35:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,16 +43,16 @@ if [ $os == "1" ]; then
         update
         apt-get install sudo
         sudo apt-get -y install lxde
-        sudo apt-get -y install xorg lxde-core vnc4server
+        sudo apt-get -y install xorg lxde-core tightvncserver
         #sudo apt-get -y install vnc4server
-        sudo vncpasswd
+        #sudo vncpasswd
+
+        vncserver :1
+        vncserver -kill :1
 
         echo "lxterminal &"  >> ~/.vnc/xstartup
         echo "/usr/bin/lxsession -s LXDE &" >> ~/.vnc/xstartup
         #echo "exec /usr/bin/mate-session &" >> ~/.vnc/xstartup
-
-        vncserver :1
-        vncserver -kill :1
 
         programms
 
